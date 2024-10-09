86ce6bdaf9b01e4ec8341d1d3d1532b3ae5b140f
Gelson Antunes <gelsondeveloper@hotmail.com>
2024-10-09 17:16:37 -0300

    refactor<favicon> atualizando favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,21 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Portfólio",
+  description: "",
+  icons: {
+    icon: "/favicon.ico",
+    apple: "/apple-touch-icon.png",
+    shortcut: "/favicon-16x16.png",
+    other: [
+      {
+        rel: "icon",
+        url: "/favicon-32x32.png",
+        sizes: "32x32",
+        type: "image/png",
+      },
+    ],
+  },
+  // Outros metadados como open graph, twitter, etc., podem ser adicionados aqui
 };
 
 export default function RootLayout({
